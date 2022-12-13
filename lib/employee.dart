68709abd4ae7d9f048bd71158e43e1a79b2e8194
camilamorada <113970729+camilamorada@users.noreply.github.com>
2022-12-13 11:59:31 -0800

    Initial commit

---
+++ lib/employee.dart
@@ -0,0 +1,242 @@
+//@dart=2.9
+
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart' as http;
+import 'dashboard.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Queuing Application',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const EmployeePage(),
+    );
+  }
+}
+
+class EmployeePage extends StatefulWidget {
+  const EmployeePage({key}) : super(key: key);
+
+  @override
+  _EmployeePageState createState() => _EmployeePageState();
+}
+
+class _EmployeePageState extends State<EmployeePage> {
+  TextEditingController user = TextEditingController();
+  TextEditingController pass = TextEditingController();
+
+  Future login() async {
+    var url ="http://192.168.1.24/login/employee.php";
+    var response = await http.post(url, body: {
+      "username": user.text,
+      "password": pass.text,
+    });
+    var data = json.decode(response.body);
+    if (data.toString() == "Success") {
+
+      Fluttertoast.showToast(
+        msg: 'Login Successful',
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+        toastLength: Toast.LENGTH_SHORT,
+      );
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => DashBoard(),
+        ),
+      );
+    } else {
+      Fluttertoast.showToast(
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        msg: 'Invalid',
+        toastLength: Toast.LENGTH_SHORT,
+      );
+    }
+  }
+
+  bool isPasswordVisible = false;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Color.fromARGB(55, 31, 11, 128), Color.fromARGB(25, 14, 67, 10)])),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+
+                SizedBox(
+                  height: 15,
+                ),
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundColor: Colors.deepPurple,
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('images/login.gif'),
+                    radius: 43,
+                  ),
+                ),
+                Text(
+                  'Login Here',
+                  style: TextStyle(
+                    fontSize: 26.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Center(
+                  child: Container(
+                    height: 1,
+                    width: 0.8,
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30)
+                      .copyWith(bottom: 10),
+                  child: TextField(
+                    controller: user,
+                    style: const TextStyle(color: Colors.black, fontSize: 14.5),
+                    decoration: InputDecoration(
+                        prefixIconConstraints:
+                        const BoxConstraints(minWidth: 45),
+                        prefixIcon: const Icon(
+                          Icons.email_outlined,
+                          color: Colors.black,
+                          size: 22,
+                        ),
+                        border: InputBorder.none,
+                        hintText: 'Enter Employee ID',
+                        hintStyle: const TextStyle(
+                            color: Colors.purple, fontSize: 18),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(100).copyWith(
+                                bottomRight: const Radius.circular(0)),
+                            borderSide:
+                            const BorderSide(color: Colors.white38)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(100).copyWith(
+                                bottomRight: const Radius.circular(0)),
+                            borderSide:
+                            const BorderSide(color: Colors.purple))),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30)
+                      .copyWith(bottom: 10),
+                  child: TextField(
+                    controller: pass,
+                    style: const TextStyle(color: Colors.white, fontSize: 14.5),
+                    obscureText: isPasswordVisible ? false : true,
+                    decoration: InputDecoration(
+                        prefixIconConstraints:
+                        const BoxConstraints(minWidth: 45),
+                        prefixIcon: const Icon(
+                          Icons.lock,
+                          color: Colors.black,
+                          size: 22,
+                        ),
+                        suffixIconConstraints:
+                        const BoxConstraints(minWidth: 45, maxWidth: 46),
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              isPasswordVisible = !isPasswordVisible;
+                            });
+                          },
+                          child: Icon(
+                            isPasswordVisible
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            color: Colors.white70,
+                            size: 22,
+                          ),
+                        ),
+                        border: InputBorder.none,
+                        hintText: 'Enter Password',
+                        hintStyle: const TextStyle(
+                            color: Colors.purple, fontSize: 18),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(100).copyWith(
+                                bottomRight: const Radius.circular(0)),
+                            borderSide:
+                            const BorderSide(color: Colors.white38)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(100).copyWith(
+                                bottomRight: const Radius.circular(0)),
+                            borderSide:
+                            const BorderSide(color: Colors.purple))),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    login();
+                  },
+                  child: Container(
+                    height: 53,
+                    width: double.infinity,
+                    margin: const EdgeInsets.symmetric(horizontal: 30),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 4,
+                              color: Colors.black12.withOpacity(.2),
+                              offset: const Offset(2, 2))
+                        ],
+                        borderRadius: BorderRadius.circular(100)
+                            .copyWith(bottomRight: const Radius.circular(0)),
+                        gradient: LinearGradient(colors: [
+                          Colors.deepPurpleAccent,
+                          Colors.deepPurple
+                        ])),
+                    child: Text('Login',
+                        style: TextStyle(
+                            color: Colors.white.withOpacity(.8),
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                    ),
+
+
+
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
